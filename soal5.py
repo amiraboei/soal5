abe57abcef321552397f7c149eb1885d07150755
amiraboei <amiraboei1384@gmail.com>
2024-03-05 13:04:20 -0800

    soal5

---
+++ soal5.py
@@ -0,0 +1,13 @@
+import turtle
+
+b=turtle.Screen()
+w=turtle.Turtle()
+size = int(turtle.textinput("Size", "andaze : "))
+w.forward(size)
+w.left(90)
+w.forward(size)
+w.left(90)
+w.forward(size)
+w.left(90)
+w.forward(size)
+w.left(90)
